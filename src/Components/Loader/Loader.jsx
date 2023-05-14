cbe243c733290064742f288c0359aeb7c3905629
Devarshi <devarshidwi@gmail.com>
2023-05-14 18:31:22 +0530

    first commit

---
+++ src/Components/Loader/Loader.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Loader.scss';
+import { loader } from '../../Utils/images';
+
+const Loader = () => {
+	return (
+		<div className='container'>
+			<div className='loader flex justify-center align-center'>
+				<img
+					src={loader}
+					alt=''
+				/>
+			</div>
+		</div>
+	);
+};
+
+export default Loader;
